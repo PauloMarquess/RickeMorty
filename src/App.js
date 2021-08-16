00ec182faa2920f62fb20b917731d9dca65aae94
PauloMarquess <paulinho_marques2012@hotmail.com>
2021-08-16 08:55:03 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import * as S from "./styled";
+import Banner from "./assets/banner.png"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [personagens, setPersonagem] = useState([]);
+  const [busca,setBusca] = useState ('');
+  const [filtro,setFiltro] = useState ([])
 
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then(response => response.json())
+      .then(data => setPersonagem(data.results))
+  }, []);
+
+ useEffect(() => {
+   setFiltro(
+     personagens.filter(personagem =>{
+       return personagem.name.includes(busca)
+     })
+   )
+ }
+ ,[busca,personagens])
+
+  console.log(personagens)
+
+
+
+  return (
+
+    <S.Main>
+
+      <S.Banner src= {Banner} alt="Banner Rick E Morty" width/> 
+      <S.Busca
+       placeholder="Digite o nome do personagem " 
+       onChange= { e =>{setBusca (e.target.value)} }
+      />
+
+
+
+      <S.Cards >
+        {filtro.map(personagem =>
+          <div >
+            <p>{personagem.name}</p>
+            <S.Imagens src={personagem.image} alt="Rick" />
+          
+          </div>
+        )}
+      </S.Cards>
+
+
+    </S.Main>
+  );
+
+}
 export default App;
---
+++ src/styled.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components' ;
+
+export const Banner = styled.img`
+width: 100%;
+height: 280px;
+`
+
+export const Imagens = styled.img `
+
+border-bottom-right-radius:70px;
+width: 200px;
+border:solid 1px;
+
+margin: 25px;
+
+`
+export const Cards =styled.div `
+
+display: flex;
+flex-wrap: wrap;
+padding: 20px;
+
+
+
+`
+export const Busca =styled.input `
+
+width: 300px;
+text-align: center;
+font-size: 1rem;
+border-bottom-right-radius:70px;
+margin-top: 30px;
+
+
+`
+export const Main =styled.div`
+
+text-align: center;
+flex-direction: column;
+font-family: 'Mulish', sans-serif;
+
+`
