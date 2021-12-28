b60abe640e085f7d9548180537b72ad4be5b8d49
PauloMarquess <paulinho_marques2012@hotmail.com>
2021-12-28 11:15:22 -0300

    responsivida

--- src/App.js
+++ src/App.js
@@ -1,57 +1,45 @@
 import { useState, useEffect } from "react";
 import * as S from "./styled";
-import Banner from "./assets/banner.png"
-
 
 function App() {
   const [personagens, setPersonagem] = useState([]);
-  const [busca,setBusca] = useState ('');
-  const [filtro,setFiltro] = useState ([])
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState([]);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
-      .then(response => response.json())
-      .then(data => setPersonagem(data.results))
+      .then((response) => response.json())
+      .then((data) => setPersonagem(data.results));
   }, []);
 
- useEffect(() => {
-   setFiltro(
-     personagens.filter(personagem =>{
-       return personagem.name.includes(busca)
-     })
-   )
- }
- ,[busca,personagens])
+  useEffect(() => {
+    setFiltro(
+      personagens.filter((personagem) => {
+        return personagem.name.includes(busca);
+      })
+    );
+  }, [busca, personagens]);
 
-  console.log(personagens)
-
-
+  console.log(personagens);
 
   return (
-
     <S.Main>
-
-      <S.Banner src= {Banner} alt="Banner Rick E Morty" width/> 
+      <S.Banner />
       <S.Busca
-       placeholder="Digite o nome do personagem " 
-       onChange= { e =>{setBusca (e.target.value)} }
+        placeholder="Digite o nome do personagem "
+        onChange={(e) => {
+          setBusca(e.target.value);
+        }}
       />
-
-
-
-      <S.Cards >
-        {filtro.map(personagem =>
-          <div >
+      <S.Cards>
+        {filtro.map((personagem) => (
+          <div>
             <p>{personagem.name}</p>
             <S.Imagens src={personagem.image} alt="Rick" />
-          
           </div>
-        )}
+        ))}
       </S.Cards>
-
-
     </S.Main>
   );
-
 }
 export default App;
--- src/styled.js
+++ src/styled.js
@@ -1,42 +1,55 @@
-import styled from 'styled-components' ;
+import styled from "styled-components";
+import Img from "./assets/rick.png";
+import Foto from "./assets/banner.png";
 
 export const Banner = styled.img`
-width: 100%;
-height: 280px;
-`
+  background-image: url(${Foto});
+  width: 100%;
+  height: 280px;
+  background-repeat: round;
 
-export const Imagens = styled.img `
+  @media (max-width: 769px) {
+    background-image: url(${Img});
+    background-size: contain;
+    width: 100%;
+  }
+`;
 
-border-bottom-right-radius:70px;
-width: 200px;
-border:solid 1px;
-
-margin: 25px;
-
-`
-export const Cards =styled.div `
-
-display: flex;
-flex-wrap: wrap;
-padding: 20px;
-
-
-
-`
-export const Busca =styled.input `
-
-width: 300px;
-text-align: center;
-font-size: 1rem;
-border-bottom-right-radius:70px;
-margin-top: 30px;
-outline: 2px solid gray ;
-
-`
-export const Main =styled.div`
-
-text-align: center;
-flex-direction: column;
-font-family: 'Mulish', sans-serif;
-
-`
+export const Imagens = styled.img`
+  border-bottom-right-radius: 70px;
+  width: 200px;
+  border: solid 1px;
+  margin: 0 25px;
+`;
+export const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 20px;
+  font-size: 20px;
+  font-weight: 500;
+  @media (max-width: 769px) {
+    padding: 0;
+    margin-left: 40px;
+  }
+`;
+export const Busca = styled.input`
+  width: 300px;
+  text-align: center;
+  font-size: 1rem;
+  border-bottom-right-radius: 70px;
+  margin-top: 30px;
+  outline: 2px solid gray;
+  border: none;
+  @media (max-width: 769px) {
+    width: 90%;
+    height: 30px;
+  }
+`;
+export const Main = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+  font-family: "Mulish", sans-serif;
+`;
